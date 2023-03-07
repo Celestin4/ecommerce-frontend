b96456cd50786af73d11ef359443cb3bb0e9f39d
Celestin4 <nteziryayocelestin3@gmail.com>
2023-03-07 22:31:25 +0200

    setup of the project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mysql = require("mysql")
+const dotenv = require('dotenv')
+
+const app = express();
+
+dotenv.config({ path: './.env'})
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
+db.connect((error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MySQL connected!")
+    }
+})
